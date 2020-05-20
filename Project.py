3bb91dc3b3b2044f69b906899aa7c235d8572be7
David Cisneros <dac.cisneros12@gmail.com>
2020-05-20 06:31:36 -0500

    viz

--- Project.py
+++ Project.py
@@ -13,13 +13,15 @@ from functools import reduce
 import warnings
 warnings.simplefilter('ignore')
 
-crude=pd.read_csv('Cushing, OK WTI Spot Price FOB (Dollars per Barrel).csv')
-refiner=pd.read_csv('U.S. Crude Oil Composite Acquisition Cost by Refiners (Dollars per Barrel).csv')
-usd=pd.read_csv('usd.csv')
-pan=pd.read_csv('Pandemics.csv')
-stocks=pd.read_csv('stocks.csv')
+#Opening all data from scrapping
+crude=pd.read_csv('data/Cushing, OK WTI Spot Price FOB (Dollars per Barrel).csv')
+refiner=pd.read_csv('data/U.S. Crude Oil Composite Acquisition Cost by Refiners (Dollars per Barrel).csv')
+usd=pd.read_csv('data/usd.csv')
+pan=pd.read_csv('data/Pandemics.csv')
+stocks=pd.read_csv('data/stocks.csv')
+preds=pd.read_csv('data/preds.csv')
 
-def clean(df,x):
+def clean(df,x): #This function cleans data and replace months from spanish to english.
 
     df.drop(['Apertura','Máximo','Mínimo','Vol.','% var.'],axis=1,inplace=True)
 
@@ -37,8 +39,9 @@ def clean(df,x):
 
 clean(stocks,'Stocks_price_usd')
 clean(usd,'usd')
+clean(preds,'Futuro')
 
-def clean2(df,x):
+def clean2(df,x): 
     df.rename(columns={df.columns[1]: x}, inplace=True)
     df['Date']=pd.to_datetime(df.Date)
     df= df.set_index('Date')   
@@ -46,35 +49,33 @@ def clean2(df,x):
 clean2(crude,'Crude_oil_price_usd')
 clean2(refiner,'Refiners_Cost_usd')
 
-pan=pan.fillna('non')
+pan=pan.fillna('non') #This is a boolean dataset, i had to change the data to numbers
 pan['Pandemics'] = np.where(pan['Pandemics']=='non',0,1)
 pan['Date']=pd.to_datetime(pan.Date)
 pan= pan.set_index('Date')
 
-dfs= [crude,refiner,usd,stocks,pan]
+dfs= [crude,refiner,usd,stocks,pan] #Merge all the dataframes into one
 df_merged = reduce(lambda  left,right: pd.merge(left,right,on=['Date'],how='outer'), dfs)
 df_merged.drop(crude.tail(1).index,inplace=True)
 df_merged=df_merged.set_index('Date')
 
-train, test = df_merged.Stocks_price_usd[:-10], df_merged.Stocks_price_usd[-10:]
+train, test = df_merged.Stocks_price_usd[:-10], df_merged.Stocks_price_usd[-10:] #Using SARIMAX and AR for time series regresion
+
 modelo=SARIMAX(train, order=(20, 1, 5)).fit(disp=False)
-                     
 pred=modelo.predict(len(train), len(df_merged.Stocks_price_usd)-1)     
 error=(pred-test).abs().sum()/len(pred) 
-print ('Error mae: ', error)
 res=pd.DataFrame({'real':test, 'pred':pred, 'diff':pred-test})
-print('SARIMAX DF',res)
+
 
 modelo=AR(train).fit()
 pred=modelo.predict(len(train), len(df_merged.Stocks_price_usd)-1)
 error=(pred-test).abs().sum()/len(pred)
-print ('Error mae: ', error)
 res=pd.DataFrame({'real':test, 'pred':pred, 'diff':pred-test})
-print('AR DF',res)
 
-dffinal=df_merged.copy()
 
-df_merged.reset_index(level=0, inplace=True)
+dffinal=df_merged.copy() #Making a copy for the final product
+
+df_merged.reset_index(level=0, inplace=True) #Transforming all data into numbers
 df_merged['Month']=df_merged.Date.dt.month
 df_merged['Year']=df_merged.Date.dt.year
 df_merged.drop(['Date'],axis=1,inplace=True)
@@ -83,6 +84,7 @@ X=df_merged.drop('Stocks_price_usd', axis=1)
 y=df_merged.Stocks_price_usd
 X_train, X_test, y_train, y_test=tts(X, y)
 
+#Hyper parameters of GBR
 hyper={
     'n_estimators':hp.quniform('n_estimators', 10, 10000, 10),
     
@@ -116,9 +118,8 @@ def goal(hyper):
     
     return {'loss':rmse, 'status':STATUS_OK}
 
-trials_reg=Trials()
-best=fmin(fn=goal, space=hyper, algo=tpe.suggest, max_evals=50, trials=Trials())
-print('Best hyper-parameters',best)    
+trials_reg=Trials() 
+best=fmin(fn=goal, space=hyper, algo=tpe.suggest, max_evals=50, trials=Trials())    
 
 modelo=GBR(
     n_estimators=int(best['n_estimators']),
@@ -128,7 +129,7 @@ modelo=GBR(
     validation_fraction=best['x_validation_fraction'],
     )
 
-modelo.fit(X_train, y_train)
+modelo.fit(X_train, y_train) #MSE
 y_pred=modelo.predict(X_test)
 print('Mean squared error',mse(y_test, y_pred))    
 
@@ -136,12 +137,32 @@ train_score=modelo.score(X_train, y_train) #R2
 test_score=modelo.score(X_test, y_test)
 print ('train R2:',train_score, '-- test R2:', test_score)
 
-pred=df_merged.drop('Stocks_price_usd', axis=1)
+pred=df_merged.drop('Stocks_price_usd', axis=1) #Using trained model to predict data
 res=modelo.predict(pred)
 
-dffinal.drop(['Refiners_Cost_usd','usd','Stocks_price_usd','Pandemics'],axis=1,inplace=True)
+dffinal.drop(['Refiners_Cost_usd','usd','Stocks_price_usd','Pandemics'],axis=1,inplace=True) # 3 column df to show results
 dffinal.rename(columns={'Crude_oil_price_usd': 'Real'}, inplace=True)
 dffinal['Predicitions']=res
 dffinal['Difference']=dffinal.Predicitions-dffinal.Real
 
+preds.insert(1, "Refiners_Cost_usd", [32.72, 25.80, 29.1], True) # creating df to predict prices in the future
+preds.insert(2, "usd", [99.92, 99.028, 99.48], True)
+preds.insert(3, "Pandemics", [1, 1, 1], True)
+preds.insert(0, "Crude_oil_price_usd", [29.21, 15.10, 26.5], True)
+preds.insert(4, "Month", [3, 4, 5], True)
+preds.insert(5, "Year", [2020, 2020, 2020], True)
+df2 = pd.DataFrame({"Crude_oil_price_usd":[28.4,30.7,32.9,34.5,37.1,38.1,38.5,37.1,38.3], 
+                    "Refiners_Cost_usd":[31.4,32.7,34.9,36.5,39.1,40.1,38.5,38.1,40.3],
+                    "usd":[99.4,99.62,99.51,99.6,99.5,99.85,99.53,99.62,99.81],
+                    "Month":[6,7,8,9,10,11,12,1,2],
+                    "Year":[2020,2020,2020,2020,2020,2020,2020,2021,2021],
+                    "Pandemics":[1,1,1,1,1,1,1,1,1],}) 
+preds.drop('Date',axis=1,inplace=True)
+preds=preds.append(df2, ignore_index = True)
+futuro=preds.Futuro
+preds.drop('Futuro',axis=1,inplace=True)
+res=modelo.predict(preds)
+preds['Predicitions']=res
+
 print(dffinal)
+print(preds)
---
+++ Today_oil_price.py
@@ -0,0 +1,39 @@
+import requests
+import json
+
+API_KEY = 'tYTvFTiccRfj'
+PROJECT_TOKEN = 'tTXGYJn0fKOF'
+RUN_TOKEN = 'tO1VzbSeYdh6'
+
+class Data:
+    def __init__(self,api_key,project_token):
+        self.api_key = api_key
+        self.project_token = project_token 
+        self.params = {
+            'api_key':self.api_key
+        }
+        self.get_data()
+
+
+    def get_data(self): #get the data from the API using token
+        res = requests.get(f'https://www.parsehub.com/api/v2/projects/{PROJECT_TOKEN}/last_ready_run/data', params={'api_key':API_KEY})
+        self.data = json.loads(res.text)
+
+
+    def get_today_price(self): #gets real time oil price
+        return self.data['price'] 
+
+    
+    def get_date_price(self,date): #gets oil price and var from the lasth month
+        data=self.data['last_month']
+
+        for c in data:
+            if c['date'] == date:
+                return c
+
+        return '0'        
+
+
+
+data = Data(API_KEY,PROJECT_TOKEN)
+print(data.get_date_price('15.05.2020')['close_price'])
